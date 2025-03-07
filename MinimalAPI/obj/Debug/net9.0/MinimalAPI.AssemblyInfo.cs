4fd5f34ae8479f4521048a8fb0b76e5ac0347d71
Zee <smogtongue@gmail.com>
2025-03-07 10:51:36 -0800

    Added prodcut categories and ensured prevention of null references.

--- MinimalAPI/obj/Debug/net9.0/MinimalAPI.AssemblyInfo.cs
+++ MinimalAPI/obj/Debug/net9.0/MinimalAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0021450ad11b54b8907e5fe92d53b225720903cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a776b4618f571db8abb664f403e6d6064950c604")]
 [assembly: System.Reflection.AssemblyProductAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MinimalAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
